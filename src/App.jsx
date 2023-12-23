4b7333571c44cff281fe7c7bfd5065572e91744e
Aaron Brown <aaronbrown@MacBook-Pro-2.local>
2023-12-23 13:37:56 -0800

    ListContainer component rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { hot } from 'react-hot-loader/root';
+import ListContainer from "./listContainer";
+
 
 class App extends React.Component {
   render() {
-    const { name } = this.props;
     return (
       <>
         <h1>
           DINE HARD
         </h1>
         <h4>choose wisely...</h4>
+        <ListContainer />
       </>
     );
   }
---
+++ src/listContainer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { hot } from 'react-hot-loader/root';
+
+class ListContainer extends React.Component {
+  render() {
+    return (
+
+        <div id="listContainer">
+        <ul>
+        <input type="checkbox"></input>
+        <li>Italian</li> 
+        <br></br>
+        <input type="checkbox"></input>
+        <li>Mexican</li>
+        <br></br>
+        <input type="checkbox"></input>
+        <li>Chinese</li>
+        <br></br>
+        <input type="checkbox"></input>
+        <li>Japanese</li>
+        <br></br>
+        <input type="checkbox"></input>
+        <li>French</li>
+        </ul>
+        </div>
+    )}
+}
+
+export default hot(ListContainer);
