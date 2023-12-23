a84459ccd0e5c4532a94582773d760e9e89c9af8
Aaron Brown <aaronbrown@MacBook-Pro-2.local>
2023-12-22 20:17:14 -0800

    updated html

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ import "./styles.css";
 
 
 var mountNode = document.getElementById("app");
-ReactDOM.render(<App name="Jane" />, mountNode);
+ReactDOM.render(<App name="goblin sharks" />, mountNode);
