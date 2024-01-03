39bcb33ab834679ea48d5d4f8cfffcd6f3d90c7a
user-byte123 <ewong1217@gmail.com>
2024-01-03 12:21:03 -0800

    made tables in sql

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ListContainer from "./listContainer";
+
+// main app component holds the logo and the list container component
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='body'>
+          <h1 className='header'>
+            DINE HARD
+          </h1>
+        <div className='optionsContainer'>
+          <ListContainer/>
+        </div>
+      </div>
+    );
+  };
+};
+
+export default App;
