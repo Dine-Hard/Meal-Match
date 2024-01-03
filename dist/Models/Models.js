d9e264f049f6602faa36b67be010f085ec97af5f
user-byte123 <ewong1217@gmail.com>
2024-01-03 10:46:29 -0800

    finally connected to psql turns out my connection wasnt working earlier because i had psql version 14.9 and downloaded reactlibrary version 12 because react version was older than version 18, we had version 17

--- dist/Models/Models.js
+++ dist/Models/Models.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 
 // Define the connection string for the PostgreSQL database
 const PG_URI =
-  "postgres://ppegqxkp:KJKVWTj5__WPM155G_QdDqc-m4IXfoT8@mahmud.db.elephantsql.com/ppegqxkp";
+  "postgres://kakcbadn:1FH_y5f7Cb7pv17DfC9AhXmA2MEGA_Si@mahmud.db.elephantsql.com/kakcbadn";
 
 // Create a new instance of the Pool class with the connection string
 const pool = new Pool({
