fd1824ce793c87e0840eef93f77a0e9feb7c636c
Vicky Hoang <vicky.k.hoang@gmail.com>
2024-01-03 16:05:57 -0800

    started middleware functions in controller file

--- dist/controllers/controller.js
+++ dist/controllers/controller.js
@@ -7,17 +7,35 @@ const db = require('../dist/Models/Models.js');
 // Create a controller object
 const controller = {};
 
-controller.getCusines = async (req, res, next) => {
-    try {
-        res.locals = await db.query('SELECT Cuisine FROM Cuisines')
-    } catch (err) {
-        console.log('error here');
-        return next(err);
-    } finally {
-        next();
-    }
+// controller.getCusines = async (req, res, next) => {
+//     try {
+//         res.locals = await db.query('SELECT Cuisine FROM Cuisines')
+//     } catch (err) {
+//         console.log('error here');
+//         return next(err);
+//     } finally {
+//         next();
+//     }
+// }
+
+//testing if data gets inserted into db
+controller.addUser = (req, res, next) => {
+    const text =
+        'INSERT INTO user (name)'
+        'VALUES ($1) RETURNING *';
+    const values = [
+
+    ]
 }
 
+controller.addCuisine = (req, res, next) => {
+    const text =
+        'INSERT INTO cuisines (cuisine)'
+        'VALUES ($1) RETURNING *';
+    const values = [
+
+    ]
+}
 // Define your controller methods
 controller.methodName = (req, res) => {
   // Code logic goes here
